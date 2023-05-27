7cb107d61effbf927777bd2437f4a910b739f4b1
Greg L <sincore@gmail.com>
2023-05-27 10:28:57 -0400

    updated readme file and base plugin configs.

--- __init__.py
+++ __init__.py
@@ -2,33 +2,25 @@
 Search the Laravel Documentation
 """
 
-from pathlib import Path
-
 from albert import Action, Item, QueryHandler, openUrl, info, debug
-import json
 import os
-import urllib.parse
-import html
-import re
 from algoliasearch.search_client import SearchClient
 
 md_iid = "0.5"
 md_version = "0.4"
 md_id = __name__
-md_name = "Laravel Docs"
+md_name = "Laravel"
+md_docs = "https://laravel.com/docs/"
+md_trigger = "lv"
 md_description = "Albert extension for quickly and easily searching the Laravel documentation"
-# md_license = "BSD-2"
 md_url = "https://github.com/use-the-fork/albert-laravel-docs/issues"
 md_maintainers = "@use-the-fork"
-
 
 client = SearchClient.create("E3MIRNPJH5", "1fa3a8fec06eb1858d6ca137211225c0")
 index = client.init_index("laravel")
 
-GOOGLE_ICON_PATH = "{}/google.svg".format(os.path.dirname(__file__))
-ICON_PATH = "{}/icon.svg".format(os.path.dirname(__file__))
-
-docs = "https://laravel.com/docs/"
+GOOGLE_ICON_PATH = "{}/images/google.png".format(os.path.dirname(__file__))
+ICON_PATH = "{}/images/icon.png".format(os.path.dirname(__file__))
 
 
 class Plugin(QueryHandler):
@@ -42,20 +34,20 @@ class Plugin(QueryHandler):
         return md_description
 
     def defaultTrigger(self):
-        return "lv "
+        return '{} '.format(md_trigger),
 
     def handleQuery(self, query):
             query.add(
                 Item(
-                    id=f'{md_name}/open_laravel_docs',
+                    id=f'{md_name}/open_{md_name}_docs',
                     icon=[ICON_PATH],
-                    text="Open Laravel Docs",
-                    subtext="No match found. Open laravel.com/docs...",
+                    text='Open {} Docs'.format(md_name),
+                    subtext="No match found. Open {}".format(md_docs),
                     actions=[
                         Action(
                             "Open",
-                            'Open the Laravel Documentation',
-                            lambda u=docs: openUrl(u)
+                            'Open the {} Documentation'.format(md_name.replace("https://", "")),
+                            lambda u=md_docs: openUrl(u)
                         )
 
                     ],
